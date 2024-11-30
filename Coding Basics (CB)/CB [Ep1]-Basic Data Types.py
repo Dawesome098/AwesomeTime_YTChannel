1c0fd4b77aacd0be2213d6ea7400a5c8b68d49ff
Danielle NNorom <dawesomescience@gmail.com>
2024-11-30 15:21:47 -0500

    Added Basic Data Types file
    Added the reference file used in episode 1 of AwesomeTime's coding Basics video

---
+++ Coding Basics (CB)/CB [Ep1]-Basic Data Types.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Title:"Coding Basics: Basic Data Types - booleans, numerics, strings, chars"
+Created By: Dawesome098
+"""
+
+"========Comments========"
+"""
+This file is to set you up so that you can practice some of the skills and 
+information that we talked about in the Coding Basics Series on the 
+@realAwesomeTime Youtube channel
+"""
+
+"imports"
+#None Required
+
+"Variables & input statements"
+#==========Booleans=========#
+boolean1 = True
+boolean2 = False
+
+#===========Strings=========#
+string1 = "Hello"
+string2 = "Try This One"
+substring1 = string2[:5]
+substring2 = string2[5:]
+#===========Chars===========#
+char1 = 'B'
+char2 = 'Z'
+
+#==========Numerics=========#
+integer1 = 3
+integer2 = -30
+float1 = 3.9
+float2 = -1920.023
+
+"All computaion/math"
+#None Required
+
+"All output statements"
+#==========Booleans=========#
+print("===========BOOLEANS==========")
+print("The value of boolean1 is:",boolean1)
+print("Boolean1 is of type:", type(boolean1))
+print()
+
+print("The value of boolean2 is:",boolean2)
+print("Boolean2 is of type:", type(boolean2))
+print("\n")
+
+
+#===========Strings=========#
+print("===========STRINGS==========")
+print("The value of string1 is:", string1)
+print("String1 is of type:", type(string1))
+print()
+
+print("The value of string2 is:",string2)
+print("String2 is of type:", type(string2))
+print()
+
+print("+++++SUBSTRINGS+++++")
+print("The value of substring1 is:",substring1)
+print("substring1 is of type:", type(substring1))
+print()
+
+print("The value of substring2 is:",substring2)
+print("Substring2 is of type:", type(substring2))
+print("\n")
+
+
+#===========Chars===========#
+print("===========CHARS==========")
+print("The value of char1 is:",char1)
+print("Char1 is of type:", type(char1))
+print()
+
+print("The value of char2 is:",char2)
+print("Char2 is of type:", type(char2))
+print("\n")
+
+
+#==========Numerics=========#
+print("===========NUMERICS==========")
+print("The value of integer1 is:",integer1)
+print("Integer1 is of type:",type(integer1))
+print()
+
+print("The value of integer2 is:",integer2)
+print("Integer2 is of type:",type(integer2))
+print()
+
+print("The value of float1 is:",float1)
+print("Float1 is of type:",type(float1))
+print()
+
+print("The value of float2 is:",float2)
+print("Float2 is of type:",type(float2))
+print("\n")
